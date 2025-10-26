6545851e1264a1f75a2965eb99dac8267bb60bf5
Sean Morris <640101+seanmorris@users.noreply.github.com>
2025-10-26 11:27:24 -0400

    Refactoring.

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const pi = Math.PI;
 const qpi = pi/4;
 
 // not-entirely-even square wave approximation
-const halfSquine = (x, b, c, d) => {
-	return Math.cos( Math.abs(Math.sin( d + x*pi )) ** (1 + b) ) ** c;
-}
+const halfSquine = (x, b, c) => Math.cos( Math.abs(Math.sin(qpi + x*pi)) ** (1 + b) ) ** c;
 
 export const squine = (x, r = 0) => {
 	r = Math.min(1, Math.max(0, r));
@@ -12,10 +10,11 @@ export const squine = (x, r = 0) => {
 	const c = b ** 3 + 4;
 
 	// Approximate an uneven square wave and average
-	// it with its phase-shifted inverse	
-	const i = halfSquine(-0.25 + x, b, c, qpi);
-	const j = -halfSquine(-0.25 + x, b, c, 3*qpi);
-	const s = -(1-halfSquine(0.25, b, c, qpi));
+	// it with its phase-shifted inverse
+	// then normalize the maxima to 1.
+	const i = halfSquine(-0.25 + x, b, c);
+	const j = -halfSquine(0.25 + x, b, c);
+	const s = -(1-halfSquine(0.25, b, c));
 
 	return (i+j) / s * 0.5 + 0.5;
 };
