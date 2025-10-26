306c5945f64a3d9b4beb1bbfce36e179e73406c5
Sean Morris <640101+seanmorris@users.noreply.github.com>
2025-10-26 11:04:47 -0400

    Refactor

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+const pi = Math.PI;
+const qpi = pi/4;
+
+// not-entirely-even square wave approximation
+const halfSquine = (x, b, c, d) => {
+	return Math.cos( Math.abs(Math.sin( d + x*pi )) ** (1 + b) ) ** c;
+}
+
 export const squine = (x, r = 0) => {
 	r = Math.min(1, Math.max(0, r));
 	const b = 10 * (1 - r);
 	const c = b ** 3 + 4;
 
 	// Approximate an uneven square wave and average
-	// it with its phase-shifted inverse
-	const i = Math.cos( Math.abs(Math.sin(  x * Math.PI + (Math.PI / 2)  )) ** (1 + b) ) ** c;
-	const j = 1 + -( Math.cos( Math.abs(Math.sin(  x * Math.PI  )) ** (1 + b) ) ** c);
+	// it with its phase-shifted inverse	
+	const i = halfSquine(-0.25 + x, b, c, qpi);
+	const j = -halfSquine(-0.25 + x, b, c, 3*qpi);
+	const s = -(1-halfSquine(0.25, b, c, qpi));
 
-	// Take the value at the turning point so we
-	// can scale the wave back to [0, 1]
-	const s = Math.cos( Math.sin(  0.5 * Math.PI + (Math.PI / 2)  ) ** (1 + b) ) ** c;
-	const t = 1 + -( Math.cos( Math.sin(  0.5 * Math.PI  ) ** (1 + b) ) ** c);
-	const u = (s + t) * 0.5;
-
-	return (i + j) * 0.5 / u;
-}
+	return (i+j) / s * 0.5 + 0.5;
+};
