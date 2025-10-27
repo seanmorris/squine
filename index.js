57943563364ae57bb229eea60fd9199ae2d38dfa
Sean Morris <640101+seanmorris@users.noreply.github.com>
2025-10-27 01:30:08 -0400

    Comments.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {min, max, sin, cos, abs, PI, log10} = Math;
 const qpi = PI/4;
 
-// not-entirely-even square wave approximation
+// not-entirely-symmetrical square wave approximation
 const halfSquine = (x, b, c) => cos( abs(sin(qpi + x*PI)) ** (1 + b) ) ** c;
 
 export const squine = (x, r = 0) => {
@@ -9,9 +9,9 @@ export const squine = (x, r = 0) => {
 	const b = 10 * r;
 	const c = b ** 3 + 4;
 
-	// Approximate an uneven square wave,
-	// average it with its phase-shifted inverse,
-	// then normalize the maxima to 1 & minima to 0.
+	// Calculate a halfSquine with its phase-shifted inverse,
+	// Make it symmetrical by averaging them together,
+	// Then normalize the maxima to 1 & minima to 0.
 	const i = halfSquine(-0.25 + x, b, c);
 	const j = -halfSquine(0.25 + x, b, c);
 	const s = -(1-halfSquine(0.25, b, c));
